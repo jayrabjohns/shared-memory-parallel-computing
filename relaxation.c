484e989abb71d0630fc52d21efc7eae3dfefe2a4
jayrabjohns <jay.rabjohns@gmail.com>
2023-11-03 18:34:41 +0000

    feat: parallel impl

--- relaxation.c
+++ relaxation.c
@@ -4,64 +4,18 @@
 #include <stdbool.h>
 #include <math.h>
 #include <pthread.h>
+#include <limits.h>
 
 #include "relaxation.h"
 
-void testcase_1()
-{
-    printf("testcase 1:\n");
-    size_t size = 4;
-    double test_values[4][4] = {{1.0, 1.0, 1.0, 1.0},
-                                {1.0, 0.0, 0.0, 0.0},
-                                {1.0, 0.0, 0.0, 0.0},
-                                {1.0, 0.0, 0.0, 0.0}};
-
-    run(size, &test_values);
-}
-
-void testcase_2()
-{
-    printf("testcase 2:\n");
-    size_t size = 10;
-    double test_values[10][10] = {{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0},
-                                  {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-                                  {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-                                  {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-                                  {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-                                  {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-                                  {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-                                  {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-                                  {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-                                  {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}};
-
-    run(size, &test_values);
-}
-
-void testcase_3()
-{
-    printf("testcase 3:\n");
-    static const size_t size = 100;
-    double test_values[size][size];
-
-    for (size_t j = 0; j < size; j++)
-        test_values[0][j] = 1.0f;
-
-    for (size_t i = 1; i < size; i++)
-    {
-        test_values[i][0] = 1.0f;
-        for (size_t j = 1; j < size; j++)
-            test_values[i][j] = 0.0f;
-    }
-
-    for (size_t j = 0; j < size; j++)
-        test_values[j][size - 1] = 1.0f;
-
-    run(size, &test_values);
-}
+const size_t MAX_ITERATIONS = ULONG_MAX;
 
 int main(void)
 {
-    static const size_t size = 10;
+    static const size_t size = 10000;
+    const double precision = 0.01;
+    const size_t thread_count = 8;
+
     double(*buf)[size][size];
     array_2d_alloc_no_err(size, size, &buf);
 
@@ -75,29 +29,48 @@ int main(void)
             (*buf)[i][j] = 0.0f;
     }
 
-    solve(size, buf, 1, 0.01);
+    solve(size, buf, thread_count, precision);
 
+    double(*solve_sync_buf)[size][size];
+    array_2d_alloc_no_err(size, size, &solve_sync_buf);
+
+    for (size_t j = 0; j < size; j++)
+        (*solve_sync_buf)[0][j] = 1.0f;
+
+    for (size_t i = 1; i < size; i++)
+    {
+        (*solve_sync_buf)[i][0] = 1.0f;
+        for (size_t j = 1; j < size; j++)
+            (*solve_sync_buf)[i][j] = 0.0f;
+    }
+
+    solve_sync(size, solve_sync_buf, precision);
+
+    int rc = memcmp(buf, solve_sync_buf, sizeof(*buf));
+    if (rc == 0)
+    {
+        printf("PASS solution matches synchronous implementation\n");
+    }
+    else
+    {
+        printf("FAIL solution doesn't match synchronous implementation\n");
+        printf("\nsync impl result\n");
+        array_2d_print(size, size, solve_sync_buf, stdout);
+        printf("async impl result:\n");
+        array_2d_print(size, size, buf, stdout);
+    }
+
+    free(solve_sync_buf);
     free(buf);
     return 0;
 }
 
-void run(size_t size, double (*values)[size][size])
-{
-    double(*buf)[size][size];
-    array_2d_alloc_no_err(size, size, &buf);
-    memcpy(buf, values, sizeof(*buf));
-
-    solve(size, buf, 1, 0.01);
-
-    free(buf);
-}
-
-// TODO: malloc functions for error andling; refactor args array as single 2d array (single free)
 void solve(size_t size, double (*values)[size][size], size_t thread_count, double precision)
 {
     thread_count = (size < thread_count ? size : thread_count);
-    const size_t rows_per_thread = size / thread_count;
-    const size_t rows_last_thread = size % thread_count;
+    const size_t rows_per_thread = (size - 2) / thread_count;
+    // With this way of assigning rows per thread, there's an edge case where (thread_count - 2) > size and all work get delegates the final thread (since the division gets rounded to 0)
+    // This should be fine, because in any reasonable case where this would be noticeable, the size of the problem will be far higher than the number of cores available (asusming one thread per core)
 
     double(*prev_values)[size][size];
     array_2d_alloc_no_err(size, size, &prev_values);
@@ -118,18 +91,20 @@ void solve(size_t size, double (*values)[size][size], size_t thread_count, doubl
         args[i].values = (double *)values;
         args[i].prev_values = (double *)prev_values;
         args[i].precision = precision;
-        args[i].start_row = 1 + i * rows_per_thread;
-        args[i].end_row = args[i].start_row + (i == thread_count - 1 ? rows_last_thread : rows_per_thread);
-        *(args[i].converged) = true;
+        args[i].start_row = i * rows_per_thread + 1;
+        args[i].end_row = (i == thread_count - 1 ? (size - 1) : (args[i].start_row + rows_per_thread));
+        args[i].converged = true;
+
+        printf("thread: %ld; start_row: %ld; end_row: %ld\n", i, args[i].start_row, args[i].end_row);
     }
 
     size_t iterations = 0;
     bool converged = false;
-    while (!converged)
+    while (!converged && iterations < MAX_ITERATIONS)
     {
         for (size_t i = 0; i < thread_count; i++)
         {
-            *(args[i].converged) = true;
+            args[i].converged = true;
             args[i].values = (double *)values;
             args[i].prev_values = (double *)prev_values;
             pthread_create(&handles[i], NULL, (void *(*)(void *))solve_chunk, &args[i]);
@@ -140,21 +115,28 @@ void solve(size_t size, double (*values)[size][size], size_t thread_count, doubl
             pthread_join(handles[i], NULL);
         }
 
-        ++iterations;
-        printf("iteration: %ld\n", iterations);
-        array_2d_print(size, size, values, stdout);
-        memcpy(prev_values, values, sizeof(*prev_values));
-
         converged = true;
-        for (size_t i = 0; i < thread_count; i++)
+        for (size_t row = 0; row < size && converged; row++)
         {
-            if (*(args->converged) == false)
+            for (size_t col = 0; col < size && converged; col++)
             {
-                converged = false;
-                break;
+                const double diff = fabs((*values)[row][col] - (*prev_values)[row][col]);
+                if (diff > precision)
+                    converged = false;
             }
         }
+
+        ++iterations;
+        // printf("iteration: %ld\n", iterations);
+        // array_2d_print(size, size, values, stdout);
+        memcpy(prev_values, values, sizeof(*prev_values));
     }
+
+    printf("[ASYNC] solved in %ld iterations\n", iterations);
+
+    free(prev_values);
+    free(handles);
+    free(args);
 }
 
 void *solve_chunk(solve_args *args)
@@ -162,7 +144,7 @@ void *solve_chunk(solve_args *args)
     double(*values)[args->size][args->size] = (double(*)[args->size][args->size])args->values;
     double(*prev_values)[args->size][args->size] = (double(*)[args->size][args->size])args->prev_values;
 
-    for (size_t row = args->start_row; row < args->end_row - 1; row++)
+    for (size_t row = args->start_row; row < args->end_row; row++)
     {
         for (size_t col = 1; col < args->size - 1; col++)
         {
@@ -174,6 +156,7 @@ void *solve_chunk(solve_args *args)
                 args->converged = false;
         }
     }
+
     return NULL;
 }
 
@@ -185,7 +168,7 @@ void solve_sync(size_t size, double (*values)[size][size], double precision)
 
     size_t iterations = 0;
     bool converged = false;
-    while (!converged)
+    while (!converged && iterations < MAX_ITERATIONS)
     {
         converged = true;
         for (size_t row = 1; row < size - 1; row++)
@@ -202,17 +185,14 @@ void solve_sync(size_t size, double (*values)[size][size], double precision)
         }
 
         ++iterations;
-        printf("iteration: %ld\n", iterations);
+        // printf("iteration: %ld\n", iterations);
         // array_2d_print(size, size, values, stdout);
         memcpy(prev_values, values, sizeof(*prev_values));
     }
 
-    FILE *file = fopen("log", "w");
-    if (file)
-    {
-        array_2d_print(size, size, values, file);
-        fclose(file);
-    }
+    printf("[SYNC] solved in %ld iterations\n", iterations);
+
+    free(prev_values);
 }
 
 void array_2d_alloc_no_err(size_t rows, size_t cols, double (**values)[rows][cols])
@@ -240,20 +220,6 @@ bool try_alloc_thread_handlers(size_t thread_count, pthread_t **handles, solve_a
         fprintf(stderr, "Cannot allocate memory for thread args.\n");
         free(*handles);
         return false;
-    }
-
-    bool *converged_flags = malloc(thread_count * sizeof(bool));
-    if (converged_flags == NULL)
-    {
-        fprintf(stderr, "Cannot allocate memory for convergence_flags.\n");
-        free(*handles);
-        free(*args);
-        return false;
-    }
-
-    for (size_t i = 0; i < thread_count; i++)
-    {
-        (*args)[i].converged = &converged_flags[i];
     }
 
     return true;
--- relaxation.h
+++ relaxation.h
@@ -13,19 +13,15 @@ struct solve_args
     double precision;
     size_t start_row;
     size_t end_row;
-    bool *converged;
+    bool converged;
 };
-
-void run(size_t size, double (*values)[size][size]);
 
 void solve(size_t size, double (*values)[size][size], size_t thread_count, double precision);
 void *solve_chunk(solve_args *args);
+void solve_sync(size_t size, double (*values)[size][size], double precision);
 
 void array_2d_alloc_no_err(size_t rows, size_t cols, double (**values)[rows][cols]);
 void array_2d_print(size_t rows, size_t cols, double (*array)[rows][cols], FILE *stream);
 bool try_alloc_thread_handlers(size_t thread_count, pthread_t **handles, solve_args **args);
-
-// void panic(const char *error_str);
-// void panic_malloc();
 
 #endif
